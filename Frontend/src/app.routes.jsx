af86c7d57115ba95a48cf094f7e0b90dcc0928c2
Sameer <sk6586418@gmail.com>
2026-03-26 20:29:21 +0530

    feat: replace GetMe component with Profile component in routing

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
-import GetMe from "./features/auth/pages/GetMe";
+import Profile from "./features/profiles/pages/Profile";
 import SetupProfile from "./features/auth/pages/SetupProfile";
 import Feed from "./features/post/pages/Feed";
 import CreatePost from "./features/post/pages/CreatePost";
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/get-me",
-    element: <GetMe />,
+    element: <Profile />,
   },
   {
     path: "/create-post",
