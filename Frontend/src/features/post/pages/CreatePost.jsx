f74fa79fc47fd059012f2801accc2ad17e5f694c
Sameer <sk6586418@gmail.com>
2026-03-30 14:59:23 +0530

    feat: add CreatePost component for post creation functionality

---
+++ Frontend/src/features/post/pages/CreatePost.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <main>
+        <h1>
+            create post
+        </h1>
+    </main>
+  )
+}
+
+export default CreatePost
