de259f7db1a6b6febd820a88e3aa5873ecab61b3
Sameer <sk6586418@gmail.com>
2026-04-10 19:18:15 +0530

    style: reorganize routes in app.routes.jsx for improved structure and clarity

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "./MainLayout";
+
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Profile from "./features/profiles/pages/Profile";
 import SetupProfile from "./features/auth/pages/SetupProfile";
 import Feed from "./features/post/pages/Feed";
 import CreatePost from "./features/post/pages/CreatePost";
+import LoadingPage from "./features/shared/components/LoadingPage";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Feed />,
+    path: "/loading",
+    element: <LoadingPage />,
   },
   {
     path: "/login",
@@ -24,15 +27,25 @@ export const router = createBrowserRouter([
     element: <SetupProfile />,
   },
   {
-    path: "/get-me",
-    element: <Profile />,
-  },
-  {
-    path: "/create-post",
-    element: <CreatePost />,
-  },
-  {
-    path: "*",
-    element: <Feed />,
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Feed />,
+      },
+      {
+        path: "/get-me",
+        element: <Profile />,
+      },
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+      },
+      {
+        path: "*",
+        element: <Feed />,
+      },
+    ],
   },
 ]);
